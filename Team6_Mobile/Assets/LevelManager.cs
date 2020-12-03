7febca1abafb7b48832768602b1154114a8ddec1
James Burke <k1811535@kingston.ac.uk>
2020-12-03 16:52:05 +0000

    Level Selection: Early Commit
    Foundations of architecture layed (but incomplete)

---
+++ Team6_Mobile/Assets/LevelManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Team6_Mobile/Assets/selector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class selector : MonoBehaviour
+{
+    public int TargetLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene("MainLevelScene");
+    }
+}
